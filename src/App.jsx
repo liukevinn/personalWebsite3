190beeef7aa8a137bfc7b9e68e09a337e73ee723
liukevinn <kevinwliuu@gmail.com>
2024-03-14 05:29:57 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import './App.css'
-import SkillsSection from './SkillsSection'
-import Timeline from './Timeline'
-import Dropdown from './Dropdown'
+import React from 'react';
+import './App.css';
+import SkillsSection from './SkillsSection';
+import Timeline from './Timeline';
+import Dropdown from './Dropdown';
 import { Carousel } from "./components/Carousel";
 import { slides } from "./data/carouselData.json";
 
-
-
-
 const App = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
     <div className="app-container">
       <div className="profile-header">
         <img src="src/images/profilePic2.png" alt="Profile" className="profile-photo" />
-        <h1 className="h1">hey,&nbsp;&nbsp;i'm kevin.</h1>
+        <h1 className="h1" id="getToKnowMe">hey,&nbsp;&nbsp;i'm kevin.</h1>
       </div>
-      <Dropdown/>
+      
+      {/* Dropdown navigation */}
+      <Dropdown />
+      
       <div className="getToKnowMe"> 
-        <p><strong>I am a first-generation asian-american; my parents moved here from Harbin, China in 2002. I was born in Milwaukee, Wisconsin in 2005, and I have lived there all my life. In 2023, I moved to Durham, North Carolina to attend Duke University where I am studying Computer Science and Statistics. In my free time, I enjoy practicing piano, playing soccer with friends, watching the Milwaukee Buck, and traveling to new places. I am very interested in anything tech, and I am open to any opportunities where I can develop my skills. Feel free to connect with me on LinkedIn, or contact me on social media!</strong></p>
+        <p><strong>I am a first-generation Asian-American; my parents moved here from Harbin, China in 2002. I was born in Milwaukee, Wisconsin in 2005, and I have lived there all my life. In 2023, I moved to Durham, North Carolina to attend Duke University where I am studying Computer Science and Statistics. In my free time, I enjoy practicing piano, playing soccer with friends, watching the Milwaukee Bucks, and traveling to new places. I am very interested in anything tech, and I am open to any opportunities where I can develop my skills. Feel free to connect with me on LinkedIn, or contact me on social media!</strong></p>
         <SkillsSection/>
       </div>
+
+      <div id="timeLine">
         <Timeline />
-        <div className="timeline-container">
-      <h1 className='header'>Picture Carousel</h1>
-        </div>
+      </div>
+
+      <div id="pictures" className="timeline-container">
+        <h1 className='header' id="picture">Picture Carousel</h1>
         <Carousel data={slides} />
         <p>Here are some pics of me and my friends!</p>
+      </div>
+
+      {/* Back to top button */}
+      <button className="back-to-top" onClick={scrollToTop}>Back to Top</button>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -2,25 +2,41 @@ import React from 'react';
 import './css/Dropdown.css';
 
 const Dropdown = () => {
+  const handleNavigation = (event, targetId) => {
+    if (targetId === 'linkedin'){
+      window.open('https://www.linkedin.com/in/liukevin', '_blank');
+    } else if(targetId === 'instagram'){
+      window.open('https://www.instagram.com/liukevinn', '_blank');
+    } else if(targetId === 'github'){
+      window.open('https://github.com/liukevinn', '_blank');
+    } else {
+      event.preventDefault();
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  };
+
   return (
     <>
       <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet" />
       <nav role="navigation" className="primary-navigation">
         <ul>
           <li>
-            <a href="#">Shortcuts</a>
+            <a href="#getToKnowMe" onClick={(e) => handleNavigation(e, 'getToKnowMe')}>Shortcuts</a>
             <ul className="dropdown">
-              <li><a href="#getToKnowMe">Get to know me</a></li>
-              <li><a href="#">Timeline</a></li>
-              <li><a href="#">Pictures</a></li>
+              <li><a href="#getToKnowMe" onClick={(e) => handleNavigation(e, 'getToKnowMe')}>Get to know me</a></li>
+              <li><a href="#timeLine" onClick={(e) => handleNavigation(e, 'timeLine')}>Timeline</a></li>
+              <li><a href="#pictures" onClick={(e) => handleNavigation(e, 'pictures')}>Pictures</a></li>
             </ul>
           </li>
           <li>
-            <a href="#">Contact/Socials </a>
+            <a href="#contact" onClick={(e) => handleNavigation(e, 'contact')}>Contact/Socials</a>
             <ul className="dropdown">
-              <li><a href="#">Linkedin</a></li>
-              <li><a href="#">Instagram</a></li>
-              <li><a href="#">Github</a></li>
+              <li><a href="#linkedin" onClick={(e) => handleNavigation(e, 'linkedin')}>Linkedin</a></li>
+              <li><a href="#instagram" onClick={(e) => handleNavigation(e, 'instagram')}>Instagram</a></li>
+              <li><a href="#github" onClick={(e) => handleNavigation(e, 'github')}>Github</a></li>
             </ul>
           </li>
         </ul>
